9d1f523d64530a4f0332ae760dab3775a3c222b8
Michel <baratella2@gmail.com>
2024-11-03 13:57:38 -0300

    adds error response testing

--- src/components/Users/Users.test.tsx
+++ src/components/Users/Users.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import Users from "./Users";
+import { server } from "../../mocks/server";
+import { rest } from "msw";
 
 describe("Users", () => {
   test("renders correctly", () => {
@@ -8,9 +10,20 @@ describe("Users", () => {
     expect(titleEl).toBeInTheDocument();
   });
 
-    test("renders list of users", async () => {
-      render(<Users />);
-      const usersEl = await screen.findAllByRole("listitem");
-      expect(usersEl).toHaveLength(3);
-    });
+  test("renders list of users", async () => {
+    render(<Users />);
+    const usersEl = await screen.findAllByRole("listitem");
+    expect(usersEl).toHaveLength(3);
+  });
+
+  it("renders a get error", async () => {
+    server.use(
+      rest.get("https://jsonplaceholder.typicode.com/users", (req, res, ctx) =>
+        res(ctx.status(500))
+      )
+    );
+    render(<Users />);
+    const errorEl = await screen.findByText("error fetching users");
+    expect(errorEl).toBeInTheDocument();
+  });
 });
